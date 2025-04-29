3be4728c51d0880d88a748022537bdae03c8e53a
Farzin Jabbary <farzin.jabbary@gmail.com>
2025-04-28 23:32:16 -0700

    react-query initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,21 @@ import PageNotFound from "./pages/PageNotFound"
 import GlobalStyles from "./styles/GlobalStyles"
 import AppLayout from "./ui/AppLayout"
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60,
+    },
+  },
+})
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -30,7 +42,7 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   )
 }
 
